4498f3a850d64080a2873da57c2304b65ba16730
Travis Dempsey <dempsey.travis@gmail.com>
2017-07-08 11:49:09 -0500

    expand runner class

--- lib/log_parse/runner.rb
+++ lib/log_parse/runner.rb
@@ -3,10 +3,15 @@ require 'optparse'
 module LogParse
   class Runner
     class << self
-      def default_options
-        @default_options ||= {
-          count: 'source_address',
+      def debug(msg)
+        $stderr.puts msg if options[:debug]
+      end
+
+      def options
+        @defaults ||= {
+          count:  :source_address,
           filter: { protocol: 'TLSv1' },
+          debug:  false,
         }
       end
 
@@ -15,43 +20,73 @@ module LogParse
           opts.version = LogParse::VERSION
           opts.banner = 'Usage: log_parse [options] FILE'
 
-          count_desc = "Count occurrences of FIELD. Default #{default_options[:count]}"
+          count_desc = "Count occurrences of FIELD. Default #{options[:count]}"
           opts.on('-c', '--count FIELD', LogParse::Parser.new.fields, count_desc) do |field|
-            default_options[:count] = field
+            options[:count] = field.to_sym
           end
 
-          filter_desc = "Filter by VALUE in FIELD. Default #{default_options[:filter].to_a.join('=')}"
+          filter_desc = "Filter by VALUE in FIELD. Default #{options[:filter].to_a.join('=')}"
           opts.on('-f', '--filter FIELD=VALUE', filter_desc) do |filter|
             field, value = filter.split('=')
-            raise 'Filter must be specified as FIELD=VALUE' if [field, value].any?(&:nil?)
-            default_options[:filter] = { field.to_sym => value }
+            abort 'Filter must be specified as FIELD=VALUE' if [field, value].any?(&:nil?)
+            options[:filter] = { field.to_sym => value }
+          end
+
+          debug_desc = "Run in debug mode. Default #{options[:debug]}"
+          opts.on('-d', '--debug', debug_desc) do
+            options[:debug] = true
           end
         end
       end
 
       def invoke!
         args = optparse.parse!
-        raise 'Must specify a log FILE to parse' if args.empty?
-        logfile = File.open(args.first, 'r')
-        runner = self.new(logfile, default_options[:count], default_options[:filter])
+        debug "Invoked with options #{options}"
+        abort 'Must specify a log FILE to parse' if args.empty?
+        logfile = File.open(args.first, 'r') # handle error
+        runner = self.new(logfile, options[:count], options[:filter])
         runner.invoke
-        runner.results
+        display runner.results
+      end
+
+      def display(results)
+        debug "Log parsing errors: #{results.delete(:errors)}"
+        debug "Log lines filtered: #{results.delete(:filtered)}"
+        results.sort_by{ |key, val| - val }.each do |key, val|
+          puts "#{key}: #{val}"
+        end
       end
     end
+
+    attr_reader :logfile, :count_field, :filter_field, :filter_value, :parser, :results
 
     def initialize(logfile, count, filter)
       @logfile = logfile
-      @count = count
-      @filter = filter
+      @count_field = count
+      @filter_field = filter.keys.first
+      @filter_value = filter.values.first
       @parser = LogParse::Parser.new
+      @results = Hash.new(0)
     end
 
     def invoke
-      @logfile.each do |line|
+      logfile.each do |line|
+        extracted = parser.extract(line.strip)
+        if extracted.empty?
+          self.class.debug "Error parsing #{line}"
+          add_to_results :errors
+          next
+        end
+        if extracted[filter_field] == filter_value
+          add_to_results :filtered
+          next
+        end
+        add_to_results extracted[count_field]
       end
     end
 
-    def results
+    def add_to_results(field)
+      results[field] += 1
     end
   end
 end
